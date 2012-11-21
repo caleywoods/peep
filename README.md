c726dcdfc4acb96570dc7436ba7589c99c202c2e
Caley Woods <caley@its-macbook-pro.blackynet>
2012-11-21 00:13:37 -0600

    Birth!

---
+++ README.md
@@ -0,0 +1,1 @@
+# Peep - A twitter like app in Rails 3.2 from Hartl's Tutorial
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peep::Application.config.secret_token = '2eea6e2e6b37d8bcd077711fddf5e88e729d0af51dbd0ca5fcd4bdf8acdb94a4564ed75d823a62af7af1ff1ef033b1ee2738795032670c0f4d4c663e99a60575'
